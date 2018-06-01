b6bd08360af6bbf98349629cb70b54e42b6e3aff
Anthony <anthony@anthonys-mbp.lan>
2018-05-31 21:50:31 -0400

    front end changes

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
     // this.state = { notes };
 
     // this.setState = this.setState.bind(this)
-  }
+  // }
 
   // componentDidMount() {
   //   this.newRequest();
