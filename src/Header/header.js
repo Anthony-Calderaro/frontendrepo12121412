5c387e65fabc73b361a24f307d3fa3312261806f
Anthony <anthony@anthonys-mbp.lan>
2018-05-31 21:24:35 -0400

    front end changes

--- src/Header/header.js
+++ src/Header/header.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './header.css';
 
 const Header = (props) => {
-    return (<div className='Title'></div> )}
+    return (
+        <div className='Title'>
+            <div>Content</div>
+        </div>
+    )
+}
 
 export default Header;
